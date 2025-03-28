f0e7752ae6808badec7c0ba274480d7343d8ba3c
503360078 <503360078@HC-CBKTDK3>
2025-03-28 09:39:15 +0530

    First push

---
+++ TempProject/src/test/java/ContactTest/ContactCreation.java
@@ -0,0 +1,12 @@
+package ContactTest;
+
+public class ContactCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Contact is created successfully by Pradeep");
+
+	}
+
+}
